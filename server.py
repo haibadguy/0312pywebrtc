e186ee4c5fd179495a7fcc6256a59d9a62aa4054
haibadguy <120120794+haibadguy@users.noreply.github.com>
2024-12-03 16:48:20 +0700

    Update server.py

--- server.py
+++ server.py
@@ -70,5 +70,5 @@ async def get_ice_candidates():
     return jsonify({'candidates': candidates})
 
 if __name__ == '__main__':
-    # Run Flask app
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
