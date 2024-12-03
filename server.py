be0237dca376b00643d01a590424ad4167ab489d
haibadguy <120120794+haibadguy@users.noreply.github.com>
2024-12-03 16:12:04 +0700

    Update server.py

--- server.py
+++ server.py
@@ -35,4 +35,6 @@ async def offer():
     return jsonify({'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
